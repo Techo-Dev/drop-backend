986c7b8a50b3a89ac1a4d8432458d94e6629f366
Techno-Dev <technodev294@gmail.com>
2024-10-19 14:04:56 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.use(bodyParser.urlencoded({ extended: true, parameterLimit:100000, limit: '500mb' }));
   
   app.enableCors();
   await app.listen(3000);
